f5e68a9df65da4bde5eb44689048454e6823f4fe
Rickie Natividad <rickienatividad85@gmail.com>
2022-05-19 13:39:51 -0500

    answered 3a

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -60,7 +60,11 @@ console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
-
+let starName = 0;
+while(stars[starName]){
+  console.log(stars[starName]);
+  starName++;
+}
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
