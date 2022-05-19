92da65aea024edf0376d997805d0279be015ebdc
Rickie Natividad <rickienatividad85@gmail.com>
2022-05-19 11:01:39 -0500

    1c and 1d completed.

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -27,10 +27,23 @@ console.log(i);
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
+//method 1 I started the index variable at 2 and changed the conditional to be less than/equal to 10. I added an if statement to divide by 2 and check for a remainder of 0 to ensure the numbers logged are even.
+for(i=2; i<=10; i++){
+  if(i % 2 == 0){
+    console.log(i);
+  }
+}
+
+//method 2 I set the variable i to a value of 2, changed the conditional to be less than/equal to 10 and incremented by 2 instead of by 1 to log even numbers starting at 2.
+for(i=2; i<=10; i+=2){
+  console.log(i);
+}
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-
+for(i=5; i<=5 && i>=0; i--){
+  console.log(i);
+}
 
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
